ab54c80a6ebfca1e74f7169659e1eafe154c955e
Chris Johnston <christophermichaeljohnston@gmail.com>
2016-02-24 13:59:18 -0500

    fix bugs

--- equipment/views.py
+++ equipment/views.py
@@ -21,52 +21,51 @@ class KeezersView(generic.ListView):
 def discover(request):
   global serial_device
   log = "Initializing existing equipment...\n"
-  serial_close_all()
-  fermenter_remove_dev_all()
+  serial_initialize()
+  fermenter_initialize()
   log += "Discovering new and existing equipment...\n"
   import glob
-  for file in glob.glob(DEVICE_PATH):
-    log += "Found: " + file + "\n";
-    if not serial_device[file].exists():
-      serial_open(file)
-      equipment_create_or_update(file)
+  for dev in glob.glob(DEVICE_PATH):
+    log += "Found: " + dev + "\n";
+    serial_open(dev)
+    equipment_create_or_update(dev)
   return render(request, 'equipment/discover.html', {'log': log})
 
-def serial_open(file):
+def serial_open(dev):
   global serial_device
-  serial_device[file] = serial.Serial(port=file, baudrate=9600)
+  serial_device[dev] = serial.Serial(port=dev, baudrate=9600)
   time.sleep(3)
 
-def serial_close(file):
+def serial_close(dev):
   global serial_device
-  serial_device[file].close()
+  serial_device[dev].close()
 
-def serial_close_all():
+def serial_initialize():
   global serial_device
-  for sd in serial_device:
-    serial_close(sd)
+  for dev in serial_device:
+    serial_close(dev)
   serial_device = {}
 
-def serial_cmd(file, cmd):
+def serial_cmd(dev, cmd):
   global serial_device
-  serial_device[file].flushInput()
-  serial_device[file].flushOutput()
-  serial_device[file].write(cmd.encode())
-  return serial_device[file].readline().decode().rstrip('\n')
+  serial_device[dev].flushInput()
+  serial_device[dev].flushOutput()
+  serial_device[dev].write(cmd.encode())
+  return serial_device[dev].readline().decode().rstrip('\n')
 
-def equipment_create_or_update(file):
-  type = serial_cmd(file, "getType")
+def equipment_create_or_update(dev):
+  type = serial_cmd(dev, "getType")
   if type == "FERMENTER":
-    fermenter_create_or_update(file)
+    fermenter_create_or_update(dev)
 
-def fermenter_create_or_update(file):
-  sn = serial_cmd(file, "getSN")
+def fermenter_create_or_update(dev):
+  sn = serial_cmd(dev, "getSN")
   f = Fermenter.objects.get(sn=sn)
   if not f.exists():
     f = Fermenter(sn=sn)
     f.save()
-  f.dev = file
+  f.dev = dev
   f.save()
 
-def fermenter_remove_dev_all():
+def fermenter_initialize():
   Fermenter.objects.all().update(dev=None)
