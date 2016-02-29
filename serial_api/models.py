fb1472502ac81bf1e6102cda266d13cb94f4af51
Chris Johnston <christophermichaeljohnston@gmail.com>
2016-02-29 11:44:45 -0500

    build out serial_api

--- serial_api/models.py
+++ serial_api/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class Device(models.Model):
+  device = models.CharField(max_length=16)
--- serial_api/views.py
+++ serial_api/views.py
@@ -3,40 +3,36 @@ import time
 
 DEVICE_PATH = "/dev/ttyACM*"
 
-devices = {}
-stuff = 0
+ports = {}
+
+def initialize():
+  global ports
+  for port in ports:
+    close(port)
+  ports = {}
+
+def discover(request):
+  initialize()
+  import glob
+  for device in glob.glob(DEVICE_PATH):
+    open(device)
+    type = SerialAPI.cmd(device, "getType")
+
+def open(port):
+  global ports
+  ports[port] = serial.Serial(port=port, baudrate=9600)
+  time.sleep(3)
+
+def close(port):
+  global ports
+  ports[port].close()
 
 class SerialAPI:
 
-  def discover(request):
-    initialize()
-    import glob
-    for device in glob.glob(DEVICE_PATH):
-      open(device)
-      type = cmd(device, "getType")
-      if type == "FERMENTER":
-        open(dev)
-      else:
-        close(dev)
+  def cmd(port, cmd):
+    global ports
+    ports[port].reset_input_buffer()
+    ports[port].reset_output_buffer()
+    ports[port].write((cmd+"\n").encode())
+    return ports[port].readline().decode().rstrip('\n').rstrip('\r')
 
-  def initialize():
-    global devices
-    for device in devices:
-      close(device)
-    devices = {}
-
-  def open(device):
-    global devices
-    devices[device] = serial.Serial(port=device, baudrate=9600)
-    time.sleep(3)
-
-  def close(dev):
-    global devices
-    devices[device].close()
-
-  def cmd(device, cmd):
-    global devices
-    devices[device].flushInput()
-    devices[device].flushOutput()
-    devices[device].write((cmd+"\n").encode())
-    return devices[device].readline().decode().rstrip('\n').rstrip('\r')
